76316c5231e7f4450bfdd036a93a6051c5a752aa
Tr1gEr <73477492+Tr1gEr4you@users.noreply.github.com>
2026-04-30 18:19:56 +0300

    Fix example 2

--- src/example/2/index.ts
+++ src/example/2/index.ts
@@ -1,8 +1,10 @@
 import { Application } from "../../core/application/application";
+import { jsonParser } from "../../lib/middlewares/jsonParser";
 import userRoutes from "./routes";
 
 const app = new Application();
 
+app.use({ url: "*", handler: jsonParser });
 app.use(userRoutes);
 
 app.listen({ hostname: "localhost", port: 4000 });
